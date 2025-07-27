46427561b0b861f697a06c88a9b3a69fb32eb52f
Himel-032 <139494430+Himel-032@users.noreply.github.com>
2025-07-27 20:10:04 +0600

    1 - auth controller added

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,15 @@ import dotenv from 'dotenv';
 
 import authRoutes from './routes/auth.routes.js';
 import connectMongoDB from './db/connectMongoDB.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 const app= express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); // to parse JSON bodies
+app.use(express.urlencoded({ extended: true })); // to parse URL-encoded bodies
+app.use(cookieParser()); // to parse cookies
 
 
 app.use("/api/auth", authRoutes);
